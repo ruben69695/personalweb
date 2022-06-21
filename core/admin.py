2597ba393cb52cd35501cfc40372c2fc0a455232
Ruben Arrebola <ruben.arre6@gmail.com>
2022-06-21 02:55:55 +0200

    Added new feature for projects in which they can have different technologies related to them

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,13 @@ class ContactMessageAdmin(admin.ModelAdmin):
     readonly_fields = ()
 
 class TechnologyAdmin(admin.ModelAdmin):
-    readonly_fields = ('name', 'created')
+    readonly_fields = ['created']
+
+    def get_readonly_fields(self, request, obj=None):
+        defaults = super().get_readonly_fields(request, obj=obj)
+        if obj:  # if we are updating an object
+            defaults = ['name'] + defaults
+        return defaults
 
 admin.site.register(ContactMessage, ContactMessageAdmin)
 admin.site.register(Technology, TechnologyAdmin)
